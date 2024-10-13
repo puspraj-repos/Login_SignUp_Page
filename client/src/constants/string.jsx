31cc72ac68c7972d9ff1b1e5d09053eb61c0fc8a
Aakash Nautiyal <nautiyalaakash23@gmail.com>
2024-10-13 14:55:59 +0530

    Deleted

--- client/src/constants/string.jsx
+++
@@ -1,8 +0,0 @@
-export const LOGIN = "Login";
-export const EMAIL_ADDRESS = "Email address";
-export const INVALID_MAIL = "Invalid email";
-export const PASSWORD = "Password";
-export const DONOT_HAVE_ACCESS = "Don't have the account?";
-export const REGISTER = "Register";
-
-
--- client/src/utility/commonFunctions.jsx
+++
@@ -1,7 +0,0 @@
-export const validateEmail = (text) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (emailRegex.test(text)) {
-      return false
-    };
-    return true;
-}
