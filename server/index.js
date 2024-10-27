242417d1e221afb396f8f1078b1ddea66f4a6bbd
Aditya-Nautiyal <aditya.nautiyal97@gmail.com>
2024-10-27 12:49:09 +0530

    New small change

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Determine the MongoDB URI based the on environment
+// Determine the MongoDB URI based the on environments
 const mongoURI =
   process.env.NODE_ENV === "production"
     ? process.env.MONGODB_PROD_URI
